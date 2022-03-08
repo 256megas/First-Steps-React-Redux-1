a2e4c97b1ff63cb65a0e82fc5b9b59306a07a41f
Antonio L <256megas@gmail.com>
2022-03-08 13:10:36 +0100

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// import './index.css';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+//Creating Redux components, providers and store
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import usersReducer from "./features/Users";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+});
+//End Creating Redux components, providers and store
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
